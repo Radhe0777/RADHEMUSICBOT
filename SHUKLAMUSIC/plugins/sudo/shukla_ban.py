438ef1333842c1e6d8d7471dd66fa3329a221677
Rishabh <152057079+Rishabhopi@users.noreply.github.com>
2024-10-28 10:17:53 +0530

    Update shukla_ban.py

--- SHUKLAMUSIC/plugins/sudo/shukla_ban.py
+++ SHUKLAMUSIC/plugins/sudo/shukla_ban.py
@@ -36,9 +36,9 @@ ban = ["ban", "boom"]
 unban = [
     "unban",
 ]
-mute = ["mute", "silent", "shut"]
+mute = ["mute","Rishumute", "silent", "shut"]
 unmute = ["unmute", "speak", "free"]
-kick = ["kick", "out", "nikaal", "nikal"]
+kick = ["kick","Rishukick", "out", "nikaal", "nikal"]
 promote = ["promote", "adminship"]
 demote = ["demote", "lelo"]
 group = ["group"]
@@ -48,7 +48,7 @@ channel = ["channel"]
 # ========================================= #
 
 
-@app.on_message(filters.command(["hiv", "abu", "nflex"], prefixes=["S", "B", "I"]) & admin_filter)
+@app.on_message(filters.command(["hiv", "abu", "Rflex"], prefixes=["S", "R", "I"]) & admin_filter)
 async def restriction_app(app: app, message):
     reply = message.reply_to_message
     chat_id = message.chat.id
